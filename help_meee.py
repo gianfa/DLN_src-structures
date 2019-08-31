3c3200f397f911d77fee3e43a25776973a7c247d
Gianfrancesco Angelini <gian.angelini@hotmail.com>
2019-08-31 13:32:48 +0200

    help_me

---
+++ help_meee.py
@@ -0,0 +1,38 @@
+# Build the right sentence
+# and help this program
+# to run!
+#
+# Somebody left a message here..
+
+
+ta = ['images.']
+ea = [' the ']
+t = ['left!']
+k = ['line']
+o = [' on t']
+ri = ['google']
+b = ['.com & ']
+u = ['he ']
+a = ['Go to http://']
+r = ['search']
+
+# hint:
+# Look this list is called "vars"
+# anda is made by other lists
+# with a number at their first place.
+# It looks so disordered!
+
+vars = [
+    [9, 't', t],
+    [0, 'a', a],
+    [1, 'ta', ta],
+    [5, 'ea', ea],
+    [6, 'k', k],
+    [4, 'r', r],
+    [2, 'ri',ri],
+    [3, ' b', b],
+    [7, 'o', o],
+    [8, 'u', u],
+]
+
+# now what??
